ccdd6584c576481081142bc06983f60543b8f882
root <root@gamma-sff>
2025-03-13 10:16:35 +0000

    Initial ayaneo pocket micro vendor untouched

--- vendor/bin/init.insmod.sh
+++ vendor/bin/init.insmod.sh
@@ -1,32 +1,34 @@
-#! /vendor/bin/sh
+#!/vendor/bin/sh
 
-#########################################
-### init.insmod.cfg format:           ###
-### --------------------------------- ###
-### [insmod|setprop] [path|prop name] ###
-### ...                               ###
-#########################################
+########################################################
+### init.insmod.cfg format:                          ###
+### -----------------------------------------------  ###
+### [insmod|setprop|enable/moprobe] [path|prop name] ###
+### ...                                              ###
+########################################################
 
-if [[ -e "/vendor/etc/init.insmod_charger.cfg" && "$(getprop ro.boot.mode)" == "charger" ]]; then
-  cfg_file="/vendor/etc/init.insmod_charger.cfg"
+if [ $# -eq 1 ]; then
+  cfg_file=$1
 else
-  cfg_file="/vendor/etc/init.insmod.cfg"
+  exit 1
 fi
 
 if [ -f $cfg_file ]; then
-  while IFS=" " read -r action name
+  while IFS="|" read -r action arg
   do
     case $action in
-      "insmod")
-        if [ -f $name ]; then
-          insmod $name
-        fi
-        ;;
-      "setprop") setprop $name 1 ;;
+      "insmod") insmod $arg ;;
+      "setprop") setprop $arg 1 ;;
+      "enable") echo 1 > $arg ;;
+      "modprobe")
+        case ${arg} in
+          "-b *" | "-b")
+            arg="-b $(cat /vendor/lib/modules/modules.load)" ;;
+          "*" | "")
+            arg="$(cat /vendor/lib/modules/modules.load)" ;;
+        esac
+        modprobe -a -d /vendor/lib/modules $arg ;;
     esac
   done < $cfg_file
 fi
 
-# set property even if there is no insmod config
-# as property value "1" is expected in early-boot trigger
-setprop vendor.all.modules.ready 1
