3e41881aec2fcba5474d837fe8675948bda593e0
Gamma <thegammasqueeze@gmail.com>
2025-04-27 19:29:27 +0100

    Clockspeed fixes

--- vendor/bin/setclock_powersave.sh
+++ vendor/bin/setclock_powersave.sh
@@ -1,23 +1,29 @@
 #!/system/bin/sh
 
 echo "384000000" > /sys/devices/platform/soc/soc:mm/23140000.gpu/devfreq/23140000.gpu/min_freq
-echo "850000000" > /sys/devices/platform/soc/soc:mm/23140000.gpu/devfreq/23140000.gpu/max_freq
+echo "384000000" > /sys/devices/platform/soc/soc:mm/23140000.gpu/devfreq/23140000.gpu/max_freq
 echo "powersave" > /sys/devices/platform/soc/soc:mm/23140000.gpu/devfreq/23140000.gpu/governor
 
 echo "0" > /sys/devices/system/cpu/cpufreq/policy0/scaling_fix_freq
 echo "614400" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
-echo "2184000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy0/scaling_fix_freq
 
 echo "0" > /sys/devices/system/cpu/cpufreq/policy4/scaling_fix_freq
 echo "614400" > /sys/devices/system/cpu/cpufreq/policy4/scaling_min_freq
-echo "2301000" > /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy4/scaling_setspeed
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy4/scaling_fix_freq
 
 echo "0" > /sys/devices/system/cpu/cpufreq/policy7/scaling_fix_freq
 echo "614400" > /sys/devices/system/cpu/cpufreq/policy7/scaling_min_freq
-echo "2704000" > /sys/devices/system/cpu/cpufreq/policy7/scaling_max_freq
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy7/scaling_max_freq
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy7/scaling_governor
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy7/scaling_setspeed
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy7/scaling_fix_freq
 
 echo "userspace" > /sys/class/devfreq/scene-frequency/governor
 echo "533" > /sys/class/devfreq/scene-frequency/userspace/set_freq
