3f7f335d18420e5dd8bb84b17f80ce03c7402ad5
Gamma <thegammasqueeze@gmail.com>
2025-07-04 13:01:16 +0100

    Latest GNext changes

---
+++ vendor/bin/setfan_cool.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+
+echo 40 > /sys/devices/platform/soc/soc\:odm/soc\:odm\:yt_fan/yt_fan_start
---
+++ vendor/bin/setfan_off.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+
+echo 0 > /sys/devices/platform/soc/soc\:odm/soc\:odm\:yt_fan/yt_fan_start
