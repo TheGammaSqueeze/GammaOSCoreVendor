292b0cc79a21dac78ffa3ad87cd2463b4007e4c5
Gamma <thegammasqueeze@gmail.com>
2025-07-09 00:01:33 +0100

    RP4Pro initial upload

--- vendor/bin/setfan_cool.sh
+++
@@ -1,3 +0,0 @@
-#!/system/bin/sh
-
-echo 40 > /sys/devices/platform/soc/soc\:odm/soc\:odm\:yt_fan/yt_fan_start
--- vendor/bin/setfan_off.sh
+++
@@ -1,3 +0,0 @@
-#!/system/bin/sh
-
-echo 0 > /sys/devices/platform/soc/soc\:odm/soc\:odm\:yt_fan/yt_fan_start
