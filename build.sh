d85c65fb55b26bbc211d1f79cc58a1aa78d46636
Gamma <thegammasqueeze@gmail.com>
2025-07-09 14:45:19 +0100

    sync

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -f vendor.img
-MKE2FS_CONFIG=mke2fs.conf ./mke2fs -O ^has_journal,^sparse_super -L vendor -M /vendor -m 0 -t ext4 -b 4096 vendor.img 498482
+MKE2FS_CONFIG=mke2fs.conf ./mke2fs -O ^has_journal,^sparse_super -L vendor -M /vendor -m 0 -t ext4 -b 4096 vendor.img 270000
 ./e2fsdroid -e -T 1230768000 -S selinux_contexts.txt -f vendor/ -a / vendor.img
