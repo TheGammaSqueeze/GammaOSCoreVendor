f349f4303a7f0bfce8568510b6c0efb84dc403a3
Gamma <thegammasqueeze@gmail.com>
2025-07-10 23:52:03 +0100

    Set default buttons

--- vendor/bin/setup.sh
+++ vendor/bin/setup.sh
@@ -1,1 +1,23 @@
 settings put secure sysui_qs_tiles internet,bt,performance,usbcontrollerswitch,abxy,mappingeditor,fan,deepsleepmode,rotation,dpadAnalogToggle,analogsensitivity,analogdeadzone,analogcalibration,analogaxis,rightanalogaxis,dcdimmingemulation,retroarchmenubuttonoverride
+
+mkdir -p /data/GammaPad
+
+printf '%s\n' \
+  "BTN_GAMEPAD BTN_GAMEPAD" \
+  "BTN_EAST BTN_EAST" \
+  "BTN_NORTH BTN_NORTH" \
+  "BTN_WEST BTN_WEST" \
+  "BTN_TL BTN_TL" \
+  "BTN_TR BTN_TR" \
+  "BTN_SELECT BTN_SELECT" \
+  "BTN_START BTN_START" \
+  "BTN_MODE BTN_MODE" \
+  "BTN_THUMBL BTN_THUMBL" \
+  "BTN_THUMBR BTN_THUMBR" \
+  "754 KEY_BACK" \
+  "753 KEY_ALL_APPLICATIONS" \
+  "760 KEY_MENU" \
+> /data/GammaPad/MAPPINGS
+
+stop gammapad
+start gammapad
