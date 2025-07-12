28b807dd015cdd1e2dee962935016e3a2dc679c8
Gamma <thegammasqueeze@gmail.com>
2025-07-13 00:24:58 +0100

    Stability updates

--- vendor/bin/setclock_stock.sh
+++ vendor/bin/setclock_stock.sh
@@ -2,9 +2,9 @@
 
 sleep 1
 
-echo "390000000" > /sys/devices/platform/soc/13000000.mali/devfreq/13000000.mali/min_freq
+echo "1100000000" > /sys/devices/platform/soc/13000000.mali/devfreq/13000000.mali/min_freq
 echo "1100000000" > /sys/devices/platform/soc/13000000.mali/devfreq/13000000.mali/max_freq
-echo "simple_ondemand" > /sys/devices/platform/soc/13000000.mali/devfreq/13000000.mali/governor
+echo "performance" > /sys/devices/platform/soc/13000000.mali/devfreq/13000000.mali/governor
 
 echo "500000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
 echo "2000000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
