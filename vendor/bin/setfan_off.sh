95f8cfef70e0648911cd8f0e8277e80cf96b9e3a
Gamma <thegammasqueeze@gmail.com>
2025-09-19 21:49:54 +0100

    RG476H Initial upload

--- vendor/bin/setfan_off.sh
+++
@@ -1,3 +0,0 @@
-#!/system/bin/sh
-
-echo 0 > /sys/devices/platform/soc/soc:fan/hwmon/hwmon0/pwm1
--- vendor/bin/vendor_discard.sh
+++
@@ -1,5 +0,0 @@
-#!/vendor/bin/sh
-
-echo 1 | tee /sys/fs/f2fs/dm-*/discard_granularity >/dev/null
-echo 0 | tee /sys/fs/f2fs/dm-*/discard_idle_interval >/dev/null
-echo 1 | tee /sys/fs/f2fs/dm-*/gc_urgent > /dev/null
