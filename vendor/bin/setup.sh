5ea248cbb658046824417a6b48fb3016733a2e22
Gamma <thegammasqueeze@gmail.com>
2025-11-03 00:03:56 +0000

    Set default props

---
+++ vendor/bin/setup.sh
@@ -0,0 +1,5 @@
+setprop persist.gsf.sec_rot 90
+setprop persist.gif.map.fts_ts 0
+setprop persist.gif.map.fts_ts_3 2
+setprop persist.gif.rot.fts_ts_3 90
+settings put global development_force_desktop_mode_on_external_displays 1
