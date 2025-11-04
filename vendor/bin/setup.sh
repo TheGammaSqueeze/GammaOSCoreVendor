199130a478c2f2bdabe9a53b559b9f0e64bac508
Gamma <thegammasqueeze@gmail.com>
2025-11-04 23:33:59 +0000

    Set default props

--- vendor/bin/setup.sh
+++ vendor/bin/setup.sh
@@ -3,3 +3,12 @@ setprop persist.gif.map.fts_ts 0
 setprop persist.gif.map.fts_ts_3 2
 setprop persist.gif.rot.fts_ts_3 90
 settings put global development_force_desktop_mode_on_external_displays 1
+settings put global force_desktop_mode_on_external_displays 1
+setprop persist.gammaos.secondary_home "com.android.launcher3/com.android.launcher3.secondarydisplay.SecondaryDisplayLauncher"
+settings put system default_refresh_rate 120
+settings put system min_refresh_rate 120
+settings put system peak_refresh_rate 120
+settings put global low_power 0
+settings put global match_content_frame_rate 0
+cmd display set-user-preferred-display-mode 0 1920 1080 120
+setprop persist.gammaos.refresh.lock 1
