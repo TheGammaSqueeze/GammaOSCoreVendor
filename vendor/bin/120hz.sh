9d496ff3d82d637758ad78d75faa04c6e51d8bce
Gamma <thegammasqueeze@gmail.com>
2025-12-07 10:05:54 +0000

    Screen refresh updates

---
+++ vendor/bin/120hz.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+setprop persist.gammaos.refresh.lock 0
+sleep 1
+settings put system default_refresh_rate 120
+settings put system min_refresh_rate 120
+settings put system peak_refresh_rate 120
+settings put global low_power 0
+settings put global match_content_frame_rate 0
+cmd display set-user-preferred-display-mode 0 1280 960 120
+sleep 1
+setprop persist.gammaos.refresh.lock 1
