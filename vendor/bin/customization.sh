7acbb704633a559a3d8160b6073797e45c0f4478
Gamma <thegammasqueeze@gmail.com>
2025-12-08 23:43:14 +0000

    Default props updates

---
+++ vendor/bin/customization.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+sleep 10
+until pm install /system/etc/magisk.apk; do sleep 1; done
+
+sleep 10
+until pm install /system/etc/magisk.apk; do sleep 1; done
+
+sleep 10
+until pm install /system/etc/magisk.apk; do sleep 1; done
+
+sleep 10
+until pm install /system/etc/magisk.apk; do sleep 1; done
--- vendor/bin/customizationload.sh
+++
@@ -1,3 +0,0 @@
-#!/system/bin/sh
-
-until pm install /system/etc/magisk.apk; do sleep 1; done
