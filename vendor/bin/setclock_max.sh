1acb0ce46993b079c85f72296f99d9b93853212b
Gamma <thegammasqueeze@gmail.com>
2026-04-03 23:52:18 +0100

    Add OC

--- vendor/bin/setclock_max.sh
+++ vendor/bin/setclock_max.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 
 echo performance > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/governor
-echo 800000000 > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/min_freq
-echo 800000000 > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/max_freq
+echo 900000000 > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/min_freq
+echo 900000000 > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/max_freq
 
 echo performance > /sys/devices/platform/fdf80200.rkvdec/devfreq/fdf80200.rkvdec/governor
 echo 400000000 > /sys/devices/platform/fdf80200.rkvdec/devfreq/fdf80200.rkvdec/min_freq
@@ -17,5 +17,5 @@ echo 920000000 > /sys/devices/platform/dmc/devfreq/dmc/min_freq
 echo 920000000 > /sys/devices/platform/dmc/devfreq/dmc/max_freq
 
 echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo 1992000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
-echo 1992000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+echo 2088638 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+echo 2088638 > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
--- vendor/bin/setclock_stock.sh
+++ vendor/bin/setclock_stock.sh
@@ -2,7 +2,7 @@
 
 echo simple_ondemand > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/governor
 echo 200000000 > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/min_freq
-echo 800000000 > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/max_freq
+echo 900000000 > /sys/devices/platform/fde60000.gpu/devfreq/fde60000.gpu/max_freq
 
 echo vdec2_ondemand > /sys/devices/platform/fdf80200.rkvdec/devfreq/fdf80200.rkvdec/governor
 echo 297000000 > /sys/devices/platform/fdf80200.rkvdec/devfreq/fdf80200.rkvdec/min_freq
@@ -18,4 +18,4 @@ echo 920000000 > /sys/devices/platform/dmc/devfreq/dmc/max_freq
 
 echo schedutil > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
 echo 408000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
-echo 1992000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+echo 2088638 > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
