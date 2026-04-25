a89dc4f515992e4f498554102e9d75ea16120cb0
Gamma <thegammasqueeze@gmail.com>
2026-04-25 21:57:30 +0100

    Revert overclock

--- vendor/bin/setclock_max.sh
+++ vendor/bin/setclock_max.sh
@@ -1,31 +1,25 @@
 #!/system/bin/sh
 
-# GPU: 900 MHz (raised from stock 850 MHz, exp 025 baseline)
-echo "900000000" > /sys/devices/platform/soc/soc:mm/60000000.gpu/devfreq/60000000.gpu/min_freq
-echo "900000000" > /sys/devices/platform/soc/soc:mm/60000000.gpu/devfreq/60000000.gpu/max_freq
+# GPU: 850 MHz
+echo "850000000" > /sys/devices/platform/soc/soc:mm/60000000.gpu/devfreq/60000000.gpu/min_freq
+echo "850000000" > /sys/devices/platform/soc/soc:mm/60000000.gpu/devfreq/60000000.gpu/max_freq
 echo "performance" > /sys/devices/platform/soc/soc:mm/60000000.gpu/devfreq/60000000.gpu/governor
 
-# CPU LITTLE cluster: 2100 MHz (raised from stock 2002 MHz, exp 025 baseline)
-echo "2100000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
-echo "2100000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+# CPU LITTLE cluster: 2002 MHz
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
 echo "userspace" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo "2100000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
-echo "2100000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_fix_freq
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_fix_freq
 
-# CPU BIG cluster: 2100 MHz (raised from stock 2002 MHz, exp 025 baseline)
-echo "2100000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_min_freq
-echo "2100000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_max_freq
+# CPU BIG cluster: 2002 MHz
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_min_freq
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_max_freq
 echo "userspace" > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
-echo "2100000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_setspeed
-echo "2100000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_fix_freq
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_setspeed
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_fix_freq
 
 # DDR scene-frequency unchanged
 echo "userspace" > /sys/class/devfreq/scene-frequency/governor
 echo "1866" > /sys/class/devfreq/scene-frequency/userspace/set_freq
 echo "50" > /sys/class/devfreq/scene-frequency/polling_interval
-
-#echo "0" > /sys/class/leds/sc27xx\:blue/brightness
-#echo "0" > /sys/class/leds/sc27xx\:green/brightness
-#echo "255" > /sys/class/leds/sc27xx\:red/brightness
-
-#sh -c '( sleep 5s; echo "0" > /sys/class/leds/sc27xx\:blue/brightness; echo "0" > /sys/class/leds/sc27xx\:green/brightness; echo "0" > /sys/class/leds/sc27xx\:red/brightness ) & echo "Done"' &
