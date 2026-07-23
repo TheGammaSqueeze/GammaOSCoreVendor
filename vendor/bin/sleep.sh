f82249ae11157354b5de1501e6761b2deadd273a
Gamma <thegammasqueeze@gmail.com>
2026-07-24 00:22:35 +0100

    updates

--- vendor/bin/sleep.sh
+++ vendor/bin/sleep.sh
@@ -4,6 +4,8 @@ STATE="/sys/power/state"
 MEM_SLEEP="/sys/power/mem_sleep"
 INTERVAL_SEC=10
 
+sleep 10
+
 # Prefer deep if available
 if [ -w "$MEM_SLEEP" ] && grep -q "deep" "$MEM_SLEEP" 2>/dev/null; then
   echo deep > "$MEM_SLEEP" 2>/dev/null
